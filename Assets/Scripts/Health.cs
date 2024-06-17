226b2014fd39672d75cccf7b93fdb958ae23bfe9
AbhineetBaghel <baghelabhicoc7@gmail.com>
2024-06-17 11:02:06 +0530

    added death animation

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
@@ -27,13 +27,72 @@ public class Health : MonoBehaviour
 
         if (health <= 0 )
         {
-            if(isLocalPlayer)
+            animator.SetBool("isDying", true);
+
+            if (isLocalPlayer)
+
             RoomManager.instance.SpawnPlayer();
 
-            animator.SetBool("isDying", true);
 
              Destroy(gameObject);
         }
+
+        else
+        {
+            animator.SetBool("isDying", false);
+        }
     }
 
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public bool isLocalPlayer;
+    Animator animator;
+
+    [Header("UI")]
+    public TextMeshProUGUI healthText;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    [PunRPC]
+    public void TakeDamage(int _damage)
+    {
+        health -= _damage;
+
+        healthText.text = health.ToString();
+
+        if (health <= 0)
+        {
+            animator.SetBool("isDying", true);
+
+            if (isLocalPlayer)
+            {
+                StartCoroutine(HandleDeath());
+            }
+        }
+    }
+
+    private IEnumerator HandleDeath()
+    {
+        // Assume your death animation length is 3 seconds. Adjust as necessary.
+        yield return new WaitForSeconds(3f);
+
+        // Respawn player
+        RoomManager.instance.SpawnPlayer();
+
+        // Destroy the current game object
+        Destroy(gameObject);
+    }
 }
