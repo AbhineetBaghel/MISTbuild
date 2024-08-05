eaf475d4419c8930f1691f3c09206de97d7ab811
AbhineetBaghel <baghelabhicoc7@gmail.com>
2024-08-06 00:16:14 +0530

    added gameui and fixed bugs

---
+++ Assets/Scripts/uiRedirect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiRedirect : MonoBehaviour
+{
+    public void btnclicked()
+    {
+        Debug.Log("clicked");
+    }
+}
